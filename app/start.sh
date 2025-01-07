494d15dbef9b61a449804da95f8a4d1f7e50fc59
LucasKBF <lucas.k.b.fabris@gmail.com>
2025-01-06 23:22:20 -0300

    modified:   app/start.sh 	chromedriver/

--- app/start.sh
+++ app/start.sh
@@ -1,11 +1,16 @@
-#!/bin/bash
-# Instalar dependências
+# Atualizar pacotes
 apt-get update && apt-get install -y wget unzip
-# Instalar Chrome
+
+# Instalar Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install -y ./google-chrome-stable_current_amd64.deb
-# Instalar ChromeDriver
+
+# Baixar ChromeDriver
 wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip -d /usr/bin/
-# Rodar o servidor
+unzip chromedriver_linux64.zip -d ./chromedriver_dir
+
+# Configurar PATH para incluir o diretório do ChromeDriver
+export PATH=$PATH:$(pwd)/chromedriver_dir
+
+# Iniciar o servidor
 gunicorn app:app
